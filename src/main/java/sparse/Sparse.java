fa4e97f44e24cfcd36232b16be43886ac5bbc0a3
Pablo Ortiz <paortiz@elpoli.edu.co>
2022-09-28 07:02:05 -0500

    Application refactored to be more modular

--- src/main/java/sparse/Sparse.java
+++ src/main/java/sparse/Sparse.java
@@ -4,22 +4,12 @@ import java.util.Scanner;
 
 public class Sparse {
 
-    // Sparse matrix having size 4*5  
-    private static int array[][]
-            = {
-                {0, 0, 6, 0, 9},
-                {0, 0, 4, 6, 0},
-                {0, 0, 0, 0, 0},
-                {0, 1, 2, 0, 0}
-            };
-
-    private static int compactMatrix[][];
-
+    // private static int packedMatrix[][];
     public Sparse() {
 
     }
 
-    private static void read() {
+    private static int [][] read() {
         int i, j;
 
         System.out.println("Enter total rows and columns: ");
@@ -27,7 +17,7 @@ public class Sparse {
         int row = s.nextInt();
         int column = s.nextInt();
 
-        array = new int[row][column];
+        int[][] array = new int[row][column];
 
         System.out.println("Enter matrix:");
         for (i = 0; i < row; i++) {
@@ -36,11 +26,15 @@ public class Sparse {
                 System.out.print(" ");
             }
         }
+        
+        return array;
     }
 
-    public static boolean isSparse() {
+    public static boolean isSparse(int [][] array) {
         int i, j, zero = 0, count = 0;
+        // array.length returns the number of rows
         for (i = 0; i < array.length; i++) {
+            // < array[i].length returns the number of colums of the ith row
             for (j = 0; j < array[i].length; j++) {
                 if (array[i][j] == 0) {
                     zero++;
@@ -56,7 +50,7 @@ public class Sparse {
         }
     }
 
-    public static void print() {
+    public static void print(int array[][]) {
         int i, j;
         for (i = 0; i < array.length; i++) {
             for (j = 0; j < array[i].length; j++) {
@@ -66,7 +60,9 @@ public class Sparse {
         }
     }
 
-    public static void compact() {
+    public static int[][] pack(int [][] array) {
+        int[][] packedMatrix;
+
         // Count non zero entries of the sparse matrix
         int nonZeroCount = 0;
         for (int i = 0; i < array.length; i++) {
@@ -77,45 +73,51 @@ public class Sparse {
             }
         }
 
-        // Create compact matrix
-        compactMatrix = new int[3][nonZeroCount];
+        // Create pack matrix
+        packedMatrix = new int[3][nonZeroCount];
         int k = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (array[i][j] != 0) {
-                    compactMatrix[0][k] = i;
-                    compactMatrix[1][k] = j;
-                    compactMatrix[2][k] = array[i][j];
+                    packedMatrix[0][k] = i;
+                    packedMatrix[1][k] = j;
+                    packedMatrix[2][k] = array[i][j];
                     k++;
                 }
             }
         }
 
+        return packedMatrix;
+
     }
 
-    public static void printCompactMatrix() {
-        for (int i = 0; i < compactMatrix.length; i++) {
-            for (int j = 0; j < compactMatrix[i].length; j++) {
-                System.out.print(compactMatrix[i][j] + "  ");
-            }
-            System.out.println("");
-        }
-    }
 
     public static void main(String args[]) {
+        // Sparse matrix having size 4*5  
+        
+        int array[][]
+                = {
+                    {0, 0, 6, 0, 9},
+                    {0, 0, 4, 6, 0},
+                    {0, 0, 0, 0, 0},
+                    {0, 1, 2, 0, 0}
+                };
 
         // read();
-        if (isSparse()) {
+        if (isSparse(array)) {
             System.out.println("the matrix is sparse");
         } else {
             System.out.println("the matrix is not sparse");
         }
-        System.out.println("The sparse matrix is ...");
-        print();
 
-        compact();
+        System.out.println(
+                "The sparse matrix is ...");
+        print(array);
 
-        System.out.println("The compact matrix is ...");
-        printCompactMatrix();
+        int[][] packedtMatrix = pack(array);
+
+        System.out.println(
+                "The compact matrix is ...");
+        print(packedtMatrix);
     }
 }
